a5e16e9e9ae746d162fbb4cdc2f635250eb3bdc0
branchtheory <97463314+branchtheory@users.noreply.github.com>
2025-07-21 13:57:40 +0100

    Update sharedValuesAndTools.js

--- js/sharedValuesAndTools.js
+++ js/sharedValuesAndTools.js
@@ -15,6 +15,6 @@ export function isBrokenBranch(branch) {
   if (Array.isArray(branch)) {
     return branch.length != NUMBER_OF_GRID_ITEMS || branch.some(e => !Array.isArray(e) || e.length === 0);
   } else {
-    return branch === BROKEN_BRANCH;
+    return branch === "invalid" || branch === BROKEN_BRANCH;
   }
 }
