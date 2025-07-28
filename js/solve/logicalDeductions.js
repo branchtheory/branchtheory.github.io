1b68a6fe5d923681a92c4c524c216b58b44d0ee6
branchtheory <97463314+branchtheory@users.noreply.github.com>
2025-07-28 21:57:51 +0100

    Update logicalDeductions.js

--- js/solve/logicalDeductions.js
+++ js/solve/logicalDeductions.js
@@ -131,3 +131,54 @@ function isLastOccurrenceOfGridValue(firstBranch, itemIndex, grid16Value) {
   }
   return true;
 }
+
+function linePairsCanFitInTheLine16(linePair1, linePair2, line16original) {
+  let line16 = [...line16original];
+  let linePair1OK = false;
+  let linePair2OK = false;
+  let gapAttempt1;
+
+  if (line16.includes(linePair1)) {
+    linePair1OK = true;
+  } else {
+    gapAttempt1 = fitInAGap(linePair1, line16);
+    line16 = gapAttempt1.line16;
+    linePair1OK = gapAttempt1.ok;
+  }
+
+  if (line16.includes(linePair2)) {
+    linePair2OK = true;
+  } else {
+    linePair2OK = fitInAGap(linePair2, line16).ok;
+  }
+
+  return linePair1OK && linePair2OK;
+}
+
+function fitInAGap(linePair, line16) {
+  // Check if it's smaller than the first item
+  if (line16[0] > linePair) {
+    return { "line16": line16, ok: false }
+  }
+
+  // Check if there's a gap before values that equal the linePair, or before the first one that's greater than it
+  for (let index = 1; index < line16.length; index++) {
+    if (linePair <= line16[index]) {
+      if (line16[index - 1] === BLANK_LINE_ITEM) {
+        line16.splice(index - 1, 1);
+        return { "line16": line16, ok: true }
+      }
+      if (linePair < line16[index]) {
+        break;
+      }
+    }
+  }
+
+  // Check if there's a gap at the end of the array
+  if (line16[line16.length - 1] === BLANK_LINE_ITEM) {
+    line16.splice(line16.length - 1, 1);
+    return { "line16": line16, ok: true }
+  }
+
+  return { "line16": line16, ok: false }
+}
