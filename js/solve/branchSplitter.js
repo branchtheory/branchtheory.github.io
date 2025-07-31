356018824c2ee00224c9e07f2d2baf50902b732e
branchtheory <97463314+branchtheory@users.noreply.github.com>
2025-07-31 11:31:04 +0100

    Update branchSplitter.js

--- js/solve/branchSplitter.js
+++ js/solve/branchSplitter.js
@@ -4,30 +4,30 @@ import {
   NOT_FOUND,
 } from './sharedValuesAndTools.js';
 
-export function splitFirstBranch(branchQueue, ns) {
+export function splitFirstBranch(branchQueue) {
   const indexOfGridItemToSplit = getIndexOfGridItemToSplit(branchQueue.firstBranch);
   if (indexOfGridItemToSplit === NOT_FOUND) return;
 
-  createANewBranchForEachQuad(branchQueue, indexOfGridItemToSplit, ns);
+  createANewBranchForEachQuad(branchQueue, indexOfGridItemToSplit);
   branchQueue.removeFirstBranch();
 }
 
 function getIndexOfGridItemToSplit(firstBranch) {
   for (let gridItemIndex = 0; gridItemIndex < NUMBER_OF_GRID_ITEMS; gridItemIndex++) {
-    if (firstBranch[gridItemIndex].length > 0 && firstBranch[gridItemIndex][0].status === UNUSED) {
+    if (firstBranch[gridItemIndex].length > 0 && firstBranch[gridItemIndex].some(quad => quad.status === UNUSED) {
       return gridItemIndex;
     }
   }
   return NOT_FOUND;
 }
 
-function createANewBranchForEachQuad(branchQueue, indexOfGridItemToSplit, ns) {
+function createANewBranchForEachQuad(branchQueue, indexOfGridItemToSplit) {
   let quadIndex = 0;
 
   while (quadIndex < branchQueue.firstBranch[indexOfGridItemToSplit].length) {
     const branchCopy = deepCopyABranch(branchQueue.firstBranch);
     setThisQuadToBeTheOnlyQuadInThisGridItem(branchCopy, indexOfGridItemToSplit, quadIndex);
-    branchQueue.createNewBranch(branchCopy, ns);
+    branchQueue.createNewBranch(branchCopy);
     quadIndex++;
   }
 }
