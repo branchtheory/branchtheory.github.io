6e9e985c88f2c760cc38c2d5d6feb223e4f283b8
branchtheory <97463314+branchtheory@users.noreply.github.com>
2025-08-03 17:43:44 +0100

    Update branchSplitter.js

--- js/solve/branchSplitter.js
+++ js/solve/branchSplitter.js
@@ -1,5 +1,7 @@
 import {
   NUMBER_OF_GRID_ITEMS,
+  SELECTED,
+  REJECTED,
   UNUSED,
   NOT_FOUND,
 } from './sharedValuesAndTools.js';
@@ -12,9 +14,12 @@ export function splitFirstBranch(branchQueue) {
   branchQueue.removeFirstBranch();
 }
 
-function getIndexOfGridItemToSplit(firstBranch) {
+function getIndexOfGridItemToSplit(branch) {
   for (let gridItemIndex = 0; gridItemIndex < NUMBER_OF_GRID_ITEMS; gridItemIndex++) {
-    if (firstBranch[gridItemIndex].length > 0 && firstBranch[gridItemIndex].some(quad => quad.status === UNUSED)) {
+    if (branch[gridItemIndex].some(quad => quad.status === SELECTED)) {
+      continue;
+    }
+    if (branch[gridItemIndex].filter(quad => quad.status === UNUSED).length >= 2) {
       return gridItemIndex;
     }
   }
@@ -22,24 +27,25 @@ function getIndexOfGridItemToSplit(firstBranch) {
 }
 
 function createANewBranchForEachQuad(branchQueue, indexOfGridItemToSplit) {
-  let quadIndex = 0;
+  const gridItem = branchQueue.firstBranch[indexOfGridItemToSplit];
 
-  while (quadIndex < branchQueue.firstBranch[indexOfGridItemToSplit].length) {
-    const branchCopy = deepCopyABranch(branchQueue.firstBranch);
-    setThisQuadToBeTheOnlyQuadInThisGridItem(branchCopy, indexOfGridItemToSplit, quadIndex);
-    branchQueue.createNewBranch(branchCopy);
-    quadIndex++;
-  }
+  gridItem.forEach((quad, quadIndex) => {
+    if (quad.status === UNUSED) {
+      const branchCopy = deepCopyABranch(branchQueue.firstBranch);
+      const copiedGridItem = branchCopy[indexOfGridItemToSplit];
+
+      copiedGridItem.forEach((copiedQuad, i) => {
+        copiedQuad.status = (i === quadIndex) ? UNUSED : REJECTED;
+      });
+
+      branchQueue.createNewBranch(branchCopy);
+    }
+  });
 }
 
-export function deepCopyABranch(firstBranch) {
+export function deepCopyABranch(branch) {
   const branchCopy = Array(NUMBER_OF_GRID_ITEMS).fill(null).map((_, gridItemIndex) =>
-    firstBranch[gridItemIndex].map(quad => ({ ...quad }))
+    branch[gridItemIndex].map(quad => ({ ...quad }))
   );
   return branchCopy;
-}
-
-function setThisQuadToBeTheOnlyQuadInThisGridItem(branchCopy, indexOfGridItemToSplit, quadIndex) {
-  const selectedQuad = branchCopy[indexOfGridItemToSplit][quadIndex];
-  branchCopy[indexOfGridItemToSplit] = [{ ...selectedQuad }];
 }
