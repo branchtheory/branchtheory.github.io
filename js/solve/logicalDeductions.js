958f44a527a8bded207b82f64591288e8d046cf4
branchtheory <97463314+branchtheory@users.noreply.github.com>
2025-08-06 23:40:46 +0100

    Update logicalDeductions.js

--- js/solve/logicalDeductions.js
+++ js/solve/logicalDeductions.js
@@ -1,17 +1,25 @@
 import { isBrokenBranch } from './sharedValuesAndTools.js';
 import { deduceFromSingleQuads } from './deduceFromSingleQuads.js';
 import { rejectQuadsThatCannotFitInLine16 } from './checkQuadFitWithLine16.js';
+import { deduceFromSingleOperands } from './deduceFromSingleOperands.js';
 
-export function deduce(branch, grid16, line16) {
+export function deduce(branch, grid16, line16, ns) {
   let thereMayBeFurtherDeductions = true;
 
   while (thereMayBeFurtherDeductions && !isBrokenBranch(branch)) {
-    branch = rejectQuadsThatCannotFitInLine16(branch, line16)
-    const result = deduceFromSingleQuads(branch, grid16);
+    let result;
+    branch = rejectQuadsThatCannotFitInLine16(branch, line16, ns)
+
+    result = deduceFromSingleOperands(branch, line16, ns);
     branch = result.branch;
     thereMayBeFurtherDeductions = result.furtherDeductions;
-  } 
+
+    if (isBrokenBranch(branch)) { return branch; }
+
+    result = deduceFromSingleQuads(branch, grid16, undefined, ns);
+    branch = result.branch;
+    thereMayBeFurtherDeductions = thereMayBeFurtherDeductions || result.furtherDeductions;
+  }
 
   return branch;
 }
-
