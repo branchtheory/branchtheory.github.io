5d14f43aa7aba9c310cbcc9c714160ee055abec7
branchtheory <97463314+branchtheory@users.noreply.github.com>
2025-08-14 11:59:59 +0100

    Update logicalDeductions.js

--- js/solve/logicalDeductions.js
+++ js/solve/logicalDeductions.js
@@ -1,26 +1,41 @@
 import { isBrokenBranch } from './sharedValuesAndTools.js';
 import { deduceFromSingleQuads } from './deduceFromSingleQuads.js';
 import { rejectQuadsThatCannotFitInLine16 } from './checkQuadFitWithLine16.js';
-import { deduceFromSingleOperands } from './deduceFromSingleOperands.js';
+import { deduceFromLineOperandCountMatchingGrid } from './deduceFromLineOperandCountMatchingGrid.js';
+import { isPotentialSolution } from './solutionChecker.js';
 
 export function deduce(branch, grid16, line16, ns) {
   let thereMayBeFurtherDeductions = true;
 
   while (thereMayBeFurtherDeductions && !isBrokenBranch(branch)) {
-    let result;
-   // branch = rejectQuadsThatCannotFitInLine16(branch, line16, ns)
+   // ns.tprint("=== Start of deductions");
+   // ns.tprint(branch);
 
-   // result = deduceFromSingleOperands(branch, line16, ns);
-   // branch = result.branch;
+    let result;
+  //  branch = rejectQuadsThatCannotFitInLine16(branch, line16, ns)
+
+   // ns.tprint("=== Post checking quads that cannot fit line16");
+   // ns.tprint(branch);
+
+   // if (isBrokenBranch(branch) || isPotentialSolution(branch)) { return branch; }
+
+   // result = deduceFromLineOperandCountMatchingGrid(branch, line16, ns);
+  //  branch = result.branch;
   //  thereMayBeFurtherDeductions = result.furtherDeductions;
 
-  //  if (isBrokenBranch(branch)) { return branch; }
+   // ns.tprint("Post deducing from when operand count matches grid");
+   // ns.tprint(branch);
+
+    if (isBrokenBranch(branch) || isPotentialSolution(branch)) { return branch; }
 
     result = deduceFromSingleQuads(branch, grid16, undefined, ns);
     branch = result.branch;
-    thereMayBeFurtherDeductions = /*thereMayBeFurtherDeductions ||*/ result.furtherDeductions;
-  }
+    thereMayBeFurtherDeductions = /*thereMayBeFurtherDeductions || */ result.furtherDeductions;
 
+   // ns.tprint("Post deducing from singles");
+   // ns.tprint(branch);
+
+   // if (isBrokenBranch(branch) || isPotentialSolution(branch)) { return branch; }
+  }
   return branch;
 }
-
