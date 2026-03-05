9833f41496fe8c1230214938117faec0ce98ca17
branchtheory <robert.g.silverstone@gmail.com~>
2026-03-05 19:15:39 +0000

    fixing a dumb merge

--- js/pageInteractions.js
+++ js/pageInteractions.js
@@ -88,7 +88,7 @@ document.querySelectorAll('.big-input, .operand-input, .operation-input').forEac
 });
 
 document.addEventListener('keydown', function(e) {
-    if (e.key === 'Enter' || e.key === ' ' || e.key === "Tab") {
+    if (e.key === "Tab") {
         moveToNextCell(e);
     }
 });
